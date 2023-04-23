66f59af2fc0144348dbc9a21a8fa3ebc2d8f172d
Sripad <sripadmanabans@gmail.com>
2023-04-23 14:05:05 +0530

    Enabling android test.

--- theme/public/src/androidTest/kotlin/com/adjectivemonk2/pixels/theme/ThemeTest.kt
+++ theme/public/src/androidTest/kotlin/com/adjectivemonk2/pixels/theme/ThemeTest.kt
@@ -1,39 +1,38 @@
 package com.adjectivemonk2.pixels.theme
 
-// FIXME: Enable test once we have a fix for https://github.com/Sripadmanabans/Pixels/issues/45
-// import androidx.compose.material3.Text
-// import androidx.compose.runtime.collectAsState
-// import androidx.compose.ui.test.assertIsDisplayed
-// import androidx.compose.ui.test.onNodeWithText
-// import de.mannodermaus.junit5.compose.createComposeExtension
-// import kotlinx.coroutines.flow.MutableStateFlow
-// import org.junit.jupiter.api.DisplayName
-// import org.junit.jupiter.api.Test
-// import org.junit.jupiter.api.extension.RegisterExtension
-//
-// internal class ThemeTest {
-//
-//  @Suppress("JUnit5MalformedExtensions")
-//  @JvmField
-//  @RegisterExtension
-//  val extension = createComposeExtension()
-//
-//  @Test
-//  @DisplayName("Testing theme change does not affect text")
-//  fun testTheme() {
-//    extension.runComposeTest {
-//      val themeIsDark = MutableStateFlow(false)
-//      setContent {
-//        PixelsTheme(
-//          isDarkTheme = themeIsDark.collectAsState().value,
-//        ) {
-//          Text(text = "Android")
-//        }
-//      }
-//
-//      onNodeWithText("Android").assertIsDisplayed()
-//      themeIsDark.value = true
-//      onNodeWithText("Android").assertIsDisplayed()
-//    }
-//  }
-// }
+import androidx.compose.material3.Text
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.onNodeWithText
+import de.mannodermaus.junit5.compose.createComposeExtension
+import kotlinx.coroutines.flow.MutableStateFlow
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.RegisterExtension
+
+internal class ThemeTest {
+
+  @Suppress("JUnitMalformedDeclaration")
+  @JvmField
+  @RegisterExtension
+  val extension = createComposeExtension()
+
+  @Test
+  @DisplayName("Testing theme change does not affect text")
+  fun testTheme() {
+    extension.runComposeTest {
+      val themeIsDark = MutableStateFlow(false)
+      setContent {
+        PixelsTheme(
+          isDarkTheme = themeIsDark.collectAsState().value,
+        ) {
+          Text(text = "Android")
+        }
+      }
+
+      onNodeWithText("Android").assertIsDisplayed()
+      themeIsDark.value = true
+      onNodeWithText("Android").assertIsDisplayed()
+    }
+  }
+}
